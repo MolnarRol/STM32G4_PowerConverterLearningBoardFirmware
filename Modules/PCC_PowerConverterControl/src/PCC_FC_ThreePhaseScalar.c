73e4b0be7006eadae7acf09e7683b1ec03770890
Roland Molnar <molnarroland99@gmail.com>
2025-02-16 10:04:22 +0100

    3 phase scalar control initialization

---
+++ Modules/PCC_PowerConverterControl/src/PCC_FC_ThreePhaseScalar.c
@@ -0,0 +1,19 @@
+/*
+ * PCC_FC_ThreePhaseScalar.c
+ *
+ *  Created on: Feb 16, 2025
+ *      Author: molnar
+ */
+#include "PCC_private_interface.h"
+#include "UTIL_public_interface.h"
+
+static void PCC_FC_ThreePhaseScalar_Init_v(void);
+static void PCC_FC_ThreePhaseScalar_Start_v(void);
+static void PCC_FC_ThreePhaseScalar_ActiveHandling_v(void);
+static void PCC_FC_ThreePhaseScalar_Stop_v(void);
+static void PCC_FC_SimpleThreePhaseScalar_DeInit_v(void);
+
+
+
+
+
