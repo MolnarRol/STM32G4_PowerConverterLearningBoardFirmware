1f3a77433b0458038a97067c12b7cb03b8423f0e
Roland Molnar <molnarroland99@gmail.com>
2025-03-02 19:48:09 +0100

    Added button handling.

--- UI/src/ui/actions.c
+++ UI/src/ui/actions.c
@@ -12,13 +12,36 @@
 extern lv_indev_t *            input_encoder_ps;
 extern lv_indev_t *            input_push_btn_ps;
 
+void action_start_topology(lv_event_t *e);
+void action_stop_topology(lv_event_t *e);
+
+void remove_all_indev_events(lv_indev_t * indev_p)
+{
+    for(u32 event_count_u32 = lv_indev_get_event_count(input_push_btn_ps); event_count_u32 > 0; event_count_u32--)
+    {
+        lv_indev_remove_event(indev_p, event_count_u32 - 1);
+    }
+}
+
 void action_back_to_main_menu(lv_event_t *e) {
+    remove_all_indev_events(input_push_btn_ps);
     lv_indev_set_group(input_encoder_ps, groups.MainGroup);
     loadScreen(SCREEN_ID_MAIN);
 }
 
+void action_stop_topology(lv_event_t *e)
+{
+    PCC_StopActiveTopology_v();
+    remove_all_indev_events(input_push_btn_ps);
+    lv_indev_add_event_cb(input_push_btn_ps, action_back_to_main_menu, LV_EVENT_PRESSED, NULL);
+    lv_indev_add_event_cb(input_push_btn_ps, action_start_topology, LV_EVENT_LONG_PRESSED, NULL);
+}
+
 void action_start_topology(lv_event_t *e) {
-    __NOP();
+    remove_all_indev_events(input_push_btn_ps);
+    PCC_StartActiveTopology_v();
+
+    lv_indev_add_event_cb(input_push_btn_ps, action_stop_topology, LV_EVENT_PRESSED, NULL);
 }
 
 void action_go_to_power_topology_menu(lv_event_t * e)
@@ -30,7 +53,7 @@ void action_go_to_power_topology_menu(lv_event_t * e)
 
     PCC_SetTopology_b(PCC_TOPO_SinglePWM_e);
     PCC_InitializeActiveTopology_b();
-    PCC_StartActiveTopology_v();
+
 
     loadScreen(SCREEN_ID_SIMPLE_PWM);
 }
